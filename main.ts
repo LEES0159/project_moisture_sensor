6a108979d3122a3eb41d3576e7e758e06bf61803
LEES0159 <31896349+LEES0159@users.noreply.github.com>
2022-10-16 17:21:19 +0800

    without radio version

--- main.ts
+++ main.ts
@@ -1,12 +1,6 @@
 let ms0 = 0
-serial.writeLine("\"ms0 ON\"")
-radio.setGroup(1)
-radio.setFrequencyBand(7)
-radio.setTransmitPower(7)
 basic.forever(function () {
     ms0 = pins.analogReadPin(AnalogPin.P0)
-    serial.writeString("" + (pins.analogReadPin(AnalogPin.P0)))
-    radio.sendValue("\"ms0\"", ms0)
     datalogger.log(datalogger.createCV("ms0", ms0))
     if (ms0 <= 300) {
         basic.showIcon(IconNames.Yes)
