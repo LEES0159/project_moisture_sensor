34c9e80ffa0cc9dabf4627aca98a2df182006376
LEES0159 <31896349+LEES0159@users.noreply.github.com>
2022-10-16 17:18:37 +0800

    with radio version

--- main.ts
+++ main.ts
@@ -5,19 +5,13 @@ radio.setFrequencyBand(7)
 radio.setTransmitPower(7)
 basic.forever(function () {
     ms0 = pins.analogReadPin(AnalogPin.P0)
+    serial.writeString("" + (pins.analogReadPin(AnalogPin.P0)))
+    radio.sendValue("\"ms0\"", ms0)
+    datalogger.log(datalogger.createCV("ms0", ms0))
     if (ms0 <= 300) {
         basic.showIcon(IconNames.Yes)
     } else {
         basic.showIcon(IconNames.No)
     }
-    radio.sendValue("\"ms0\"", pins.analogReadPin(AnalogPin.P0))
-    serial.writeString("" + (pins.analogReadPin(AnalogPin.P0)))
-    datalogger.log(datalogger.createCV("ms0", [control.eventTimestamp(), pins.analogReadPin(AnalogPin.P0)]))
-    basic.pause(5000)
-})
-basic.forever(function () {
-    radio.sendValue("\"ms0\"", pins.analogReadPin(AnalogPin.P0))
-    serial.writeString("" + (pins.analogReadPin(AnalogPin.P0)))
-    datalogger.log(datalogger.createCV("ms0", [control.eventTimestamp(), pins.analogReadPin(AnalogPin.P0)]))
     basic.pause(5000)
 })
