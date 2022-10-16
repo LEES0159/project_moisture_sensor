99b4f3d4e726775f991b80a2d50fd2397146800d
LEES0159 <31896349+LEES0159@users.noreply.github.com>
2022-10-16 17:45:11 +0800

    2 versions - not in use (1) send if exceed threshold (2) continuously

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
 let ms0 = 0
+serial.writeLine("\"ms0 ON\"")
+radio.setGroup(1)
+radio.setFrequencyBand(7)
+radio.setTransmitPower(7)
 basic.forever(function () {
     ms0 = pins.analogReadPin(AnalogPin.P0)
-    datalogger.log(datalogger.createCV("ms0", ms0))
-    if (ms0 <= 300) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
+    // This is to send readings only if exceed THRESHOLD
+    // To remove this and use PAUSE 5000ms to continuously send at regular intervals
+    if (ms0 >= 600) {
+        radio.sendValue("\"ms0\"", ms0)
+        serial.writeString("" + (pins.analogReadPin(AnalogPin.P0)))
     }
+})
+basic.forever(function () {
+    ms0 = pins.analogReadPin(AnalogPin.P0)
+    radio.sendValue("\"ms0\"", ms0)
+    serial.writeString("" + (pins.analogReadPin(AnalogPin.P0)))
     basic.pause(5000)
 })
